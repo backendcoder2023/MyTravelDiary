67b565a9fd68ca22381395c19f8cc02a28efa16b
Dhanashri <dhanashrinimbhorkar22@navgurukul.org>
2024-08-28 13:01:30 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -22,33 +22,3 @@ const App = () => {
 export default App;
 
 
-
-
-
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Navbar from './Landingpage/Navbar';
-// import Home from './Landingpage/Home';
-// // import About from './About';
-// import Stay from './Landingpage/Stay';
-// import Food from './Landingpage/Food';
-// import LoginSignup from './Landingpage/LoginSignup';
-// import Search from './Landingpage/Search';
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <Navbar />
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/about" element={<About />} />
-//         <Route path="/stay" element={<Stay />} />
-//         <Route path="/cuisines" element={<Food />} />
-//         <Route path="/register" element={<LoginSignup />} />
-//         <Route path="/search" element={<Search />} />
-//       </Routes>
-//     </Router>
-//   );
-// };
-
-// export default App;
